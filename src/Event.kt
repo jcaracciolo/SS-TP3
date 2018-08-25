8eb77ab7b01d0ea1a07d4dda541092ac7e827e19
marcelolynch <mlynch@itba.edu.ar>
2018-08-25 13:06:18 -0300

    Fix on constructor

--- src/Event.kt
+++ src/Event.kt
@@ -1,3 +1,3 @@
 class Event(val tc: Double, val results: Array<Result>)
 
-class Result(val particle: Particle, val newVelocity: Velocity, val collisionNumber: Int)
+class Result(val particle: Particle, val newVelocity: Vector, val collisionNumber: Int)
